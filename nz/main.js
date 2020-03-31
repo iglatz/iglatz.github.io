7d9043b43e2ff1784ecbe3cff3e1c408d37cd367
iglatz <ingomar.glatz@uibk.ac.at>
2020-03-31 22:54:18 +0200

    implement leaf let

--- nz/main.js
+++ nz/main.js
@@ -1,1 +1,14 @@
-//alert ("Hallo Welt!")
+// alert ("Hallo Welt!");
+
+//connection to map -> index.html
+let map = document.querySelector("#map") 
+
+let title=map.dataset.title;
+
+var mymap = L.map("map").setView([-44.828004, 169.634525], 13);
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(mymap);
+
