551b56cb3d568a019e320ceb28ad5936297bb945
iglatz <ingomar.glatz@uibk.ac.at>
2020-03-31 23:00:53 +0200

    map pop up

--- nz/main.js
+++ nz/main.js
@@ -12,3 +12,7 @@ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap);
 
+var marker = L.marker([-44.828004, 169.634525]).addTo(mymap);
+
+marker.bindPopup("<b>Royal Albatros Center</b><br>one of the biggest Albatros colonies").openPopup();
+
