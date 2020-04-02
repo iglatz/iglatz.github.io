6f059fd7da87a049138cc4e7461eed0f73d3e253
iglatz <ingomar.glatz@uibk.ac.at>
2020-04-02 18:23:50 +0200

    Webmapping Ende!

--- world/main.js
+++ world/main.js
@@ -1,17 +1,49 @@
 let startLayer = L.tileLayer.provider("OpenTopoMap");
 
 let map = L.map("map", {
-    center: [0,0],
+    center: [0, 0],
     zoom: 2,
     layers: [
         startLayer
     ]
 });
 
-L.control.layer({
-    "OpenTopoMap" : startLayer,
-    "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    "OpenStreetMap.BZH" : L.tileLayer.provider("OpenStreetMap.BZH"),
-    "Thunderforest.Transport" : L.tileLayer.provider("Thunderforest.Transport"),
-    "NASAGIBS.ViirsEarthAtNight2012" : L.tileLayer.provider("NASAGIBS.ViirsEarthAtNight2012"),
+let circleGroup = L.featureGroup().addTo(map);
+
+L.control.layers({
+    "OpenTopoMap": startLayer,
+    "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "OpenStreetMap.BZH": L.tileLayer.provider("OpenStreetMap.BZH"),
+    "Thunderforest.Transport": L.tileLayer.provider("Thunderforest.Transport"),
+    "NASAGIBS.ViirsEarthAtNight2012": L.tileLayer.provider("NASAGIBS.ViirsEarthAtNight2012"),
+}, {
+    "Thematische Darstellung": circleGroup
 }).addTo(map);
+
+let drawCircles = function (data) {
+    // console.log(CONFIRMED);
+    for (let i = 1; i < data.length; i++) {
+        let row = data[i];
+        //console.log(row[2],row[3]);
+        let reg = `${row[0]} ${row[1]}`;
+        let lat = row[2];
+        let lng = row[3];
+        let val = row[row.length - 1];
+        let mrk = L.marker([lat, lng]).addTo(map);
+        mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
+
+        // A = r²*PI
+        // r² = A/PI
+        // r = WURZEL(A/PI)
+        let s = 0.5;
+        let r = Math.sqrt(val * s / Math.PI);
+        let circle = L.circleMarker([lat, lng], {
+            radius: r
+        }).addTo(circleGroup);
+        circle.bindPopup(`${reg}: ${val}`);
+    }
+};
+
+drawCircles(RECOVERD);
+//drawCircles(CONFIRMED);
+//drawCircles(DEATH);
