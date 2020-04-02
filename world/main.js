0ca104df8b5b038f2d0945ccf8f8b54fac7e70d0
iglatz <ingomar.glatz@uibk.ac.at>
2020-04-02 17:09:31 +0200

    x bug overflow

--- world/main.js
+++ world/main.js
@@ -11,7 +11,7 @@ let map = L.map("map", {
 L.control.layer({
     "OpenTopoMap" : startLayer,
     "OpenStreetMap.Mapnik" : L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    //"OpenStreetMap.BZH" : L.tileLayer.provider("OpenStreetMap.BZH"),
-    //"Thunderforest.Transport" : L.tileLayer.provider("Thunderforest.Transport"),
-    //"NASAGIBS.ViirsEarthAtNight2012" : L.tileLayer.provider("NASAGIBS.ViirsEarthAtNight2012"),
+    "OpenStreetMap.BZH" : L.tileLayer.provider("OpenStreetMap.BZH"),
+    "Thunderforest.Transport" : L.tileLayer.provider("Thunderforest.Transport"),
+    "NASAGIBS.ViirsEarthAtNight2012" : L.tileLayer.provider("NASAGIBS.ViirsEarthAtNight2012"),
 }).addTo(map);
