cdb254951468c11107117460d401158ef5b68957
iglatz <ingomar.glatz@uibk.ac.at>
2020-04-09 17:24:49 +0200

    Kreisgrößen sortieren!

--- world/main.js
+++ world/main.js
@@ -46,6 +46,10 @@ if (value == "confirmed") {
 
     circleGroup.clearLayers();
 
+    data.sort(function compareNumbers(row1,row2){
+        return row2[index] - row1[index];
+    });
+
     // console.log(CONFIRMED);
     for (let i = 1; i < data.length; i++) {
         let row = data[i];
@@ -54,6 +58,11 @@ if (value == "confirmed") {
         let lat = row[2];
         let lng = row[3];
         let val = row[index];
+
+         if (val == "0"){
+            continue;
+    }
+
         let mrk = L.marker([lat, lng]).addTo(map);
         mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
 
