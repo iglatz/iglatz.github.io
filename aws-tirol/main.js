68a3b4e9dc52d281babf7da459e9eff73aa87781
iglatz <ingomar.glatz@uibk.ac.at>
2020-04-22 22:13:08 +0200

    showJustTemp, PopUpPimped, headingsBold, dateNice

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -31,7 +31,8 @@ let awsUrl = "https://aws.openweb.cc/stations";
 let aws = L.geoJson.ajax(awsUrl, {
     filter: function(feature){
         //console.log("Feature in filter: ", feature);
-        return feature.geometry.coordinates[2] > 3000;
+        //return feature.geometry.coordinates[2] > 3000;
+        return feature.properties.LT != null;
     },
 
     // filter: function(feature) {
@@ -45,13 +46,50 @@ let aws = L.geoJson.ajax(awsUrl, {
     //     // return feature.properties.LT < 5;
     // },
     pointToLayer: function(point, latlng){
-        //console.log("point: ", point);
+        console.log("point Wetterkarte: ", point);
+        let schneeHoehe;
+        let rFeuchte;
+        let wind;
+        let temp;
+        // Lufttemperatur einbinden
+        if (point.properties.LT <= 0 || point.properties.LT == null){
+            temp = "Keine Angabe";
+        }else{
+            temp = point.properties.LT + "°C";
+        }
+        // Windgeschwindigkeit einbinden
+        if (point.properties.WG <= 0 || point.properties.WG == null){
+            wind = "Keine Angabe";
+        }else{
+            wind = point.properties.WG + "m/s";
+        }
+        // Relative Feuche einbinden
+        if (point.properties.RH <= 0 || point.properties.RH == null){
+            rFeuchte = "Keine Angabe";
+        }else{
+            rFeuchte = point.properties.RH + "%";
+        }
+        //Schneehöhe einbinden
+        if (point.properties.HS <= 0 || point.properties.HS == null){
+            schneeHoehe = "Keine Angabe";
+        }else{
+            schneeHoehe = point.properties.HS + "cm";
+        }
+
+
         let marker = L.marker (latlng).bindPopup(`<h3>${point.properties.name}</h3>
         <ul>
-        <li>Datum: ${point.properties.date}<li>
-        <li>Lufttemperatur:${point.properties.LT} °C </li>
+        <li><p class=bold>Seehöhe:</p> <br> ${point.geometry.coordinates[2]} hm</li>
+        <li><p class=bold>Position:</p><br> Lat: ${point.geometry.coordinates[1]} <br> Lng: ${point.geometry.coordinates[0]}</li>
+        <li><p class=bold>Datum:</p> <br> ${point.properties.date.substring(0, 10)}</li>
+        <li><p class=bold>Lufttemperatur:</p> <br> ${temp} </li>
+        <li><p class=bold>Windgeschwindigkeit:</p> <br> ${wind} </li>
+        <li><p class=bold>Relative Feuchte:</p> <br>${rFeuchte} </li>
+        <li><p class=bold>Schneehöhe:</p> <br>${schneeHoehe} </li>
         </ul>
+        <a target="link" href= "https://lawine.tirol.gv.at/data/grafiken/1100/standard/tag/${point.properties.plot}.png"> Hier gehts zur Grafik der Wetterstation </a>
         `);
+        
         return marker;
     }
 }).addTo(awsLayer);
